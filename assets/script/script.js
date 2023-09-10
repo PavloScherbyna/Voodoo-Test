e25e102c4536328d378ba84b62e8ce8ec0e77581
PavloShcherbyna <dprogfont@gmail.com>
2023-09-10 17:24:28 +0300

    basket v1.0

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,12 +13,11 @@ customSelect.addEventListener('click', () => {
     sircleSelect.style.transition = 'transform 0.3s ease-in-out';
 });
 
- // Змінні для відстеження поточної сторінки та загальної кількості товарів
 let currentPage = 1;
-const itemsPerPage = 12; // Кількість товарів на сторінці
-const totalItems = 461; // Загальна кількість товарів
+const itemsPerPage = 12; 
+const totalItems = 461; 
 
-// Функція для отримання списку продуктів з Shopify API та заповнення карточок товарів
+
 async function fetchAndFillProductCards(page) {
     try {
         const response = await fetch(`https://voodoo-sandbox.myshopify.com/products.json?limit=${itemsPerPage}&page=${page}`);
@@ -27,11 +26,9 @@ async function fetchAndFillProductCards(page) {
 
         const productWrapper = document.querySelector('.product_wrapper');
 
-        // Очищаємо контейнер перед додаванням нових товарів
         productWrapper.innerHTML = '';
 
         products.forEach(product => {
-            // Створення та заповнення карточок товарів, як раніше
             const productCard = document.createElement('div');
             productCard.className = 'product_card';
 
@@ -44,7 +41,12 @@ async function fetchAndFillProductCards(page) {
 
             const productName = document.createElement('div');
             productName.className = 'product_name';
-            productName.innerHTML = `<p>${product.title}</p><p>${product.variants[0].price} ${product.variants[0].price_min_currency}</p>`;
+
+            // Форматуємо ціну
+            const productPriceText = product.variants[0]?.price || '0.00';
+            const formattedPrice = formatPrice(productPriceText);
+
+            productName.innerHTML = `<p>${product.title}</p><p>${formattedPrice} KR.</p>`;
 
             const condition = document.createElement('div');
             condition.className = 'condition';
@@ -53,6 +55,19 @@ async function fetchAndFillProductCards(page) {
             const addToCartButton = document.createElement('button');
             addToCartButton.className = 'card_to_busket';
             addToCartButton.textContent = 'ADD TO CART';
+
+            // Додамо обробник події для кнопки "ADD TO CART"
+            addToCartButton.addEventListener('click', () => {
+                // Отримуємо інформацію про продукт, на який натиснули
+                const productName = product.title;
+
+                // Отримуємо ціну продукту та перетворюємо її на числовий формат (з заміною коми на крапку)
+                const productPriceText = product.variants[0]?.price || '0.00';
+                const productPrice = parseFloat(productPriceText.replace(',', '.'));
+
+                // Додавання товару до кошика
+                addToCart(productName, productPrice);
+            });
 
             cardInfo.appendChild(productName);
             cardInfo.appendChild(condition);
@@ -68,42 +83,39 @@ async function fetchAndFillProductCards(page) {
     }
 }
 
-// Функція для обробки кліків на кнопки "Наступна сторінка" та "Попередня сторінка"
-function handlePaginationButtonClick(action) {
-    if (action === 'next' && currentPage < Math.ceil(totalItems / itemsPerPage)) {
-        // Перехід на наступну сторінку
-        fetchAndFillProductCards(currentPage + 1);
-    } else if (action === 'prev' && currentPage > 1) {
-        // Перехід на попередню сторінку
-        fetchAndFillProductCards(currentPage - 1);
-    }
+// Залиште решту коду незмінним
+
+fetchAndFillProductCards(currentPage);
+
+// Функція для форматування ціни
+function formatPrice(price) {
+    const formattedPrice = parseFloat(price).toFixed(2);
+    return formattedPrice.replace('.', ' ').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 }
 
-// Викликаємо функцію для заповнення карточок товарів на початку
-fetchAndFillProductCards(currentPage);
+
 
 
 
 // Функція для створення нумерації сторінок
 function createPaginationButtons() {
     const paginationContainer = document.querySelector('.pagination');
-    paginationContainer.innerHTML = ''; // Очищаємо контейнер перед додаванням номерів сторінок
+    paginationContainer.innerHTML = ''; 
 
     const totalPages = Math.ceil(totalItems / itemsPerPage);
-    const visiblePages = 5; // Кількість видимих номерів сторінок
+    const visiblePages = 5; 
 
     let startPage = currentPage - Math.floor(visiblePages / 2);
-    startPage = Math.max(startPage, 1); // Мінімум 1
+    startPage = Math.max(startPage, 1); 
 
     let endPage = startPage + visiblePages - 1;
-    endPage = Math.min(endPage, totalPages); // Максимум загальна кількість сторінок
+    endPage = Math.min(endPage, totalPages); 
 
     if (startPage > 1) {
         const firstPageButton = document.createElement('button');
         firstPageButton.textContent = '1';
         firstPageButton.className = 'page_number';
 
-        // Додаємо обробник кліку для номеру сторінки
         firstPageButton.addEventListener('click', () => goToPage(1));
 
         paginationContainer.appendChild(firstPageButton);
@@ -120,7 +132,6 @@ function createPaginationButtons() {
         pageButton.textContent = i;
         pageButton.className = 'page_number';
 
-        // Додаємо обробник кліку для номеру сторінки
         pageButton.addEventListener('click', () => goToPage(i));
 
         paginationContainer.appendChild(pageButton);
@@ -137,28 +148,22 @@ function createPaginationButtons() {
         lastPageButton.textContent = totalPages;
         lastPageButton.className = 'page_number';
 
-        // Додаємо обробник кліку для номеру сторінки
         lastPageButton.addEventListener('click', () => goToPage(totalPages));
 
         paginationContainer.appendChild(lastPageButton);
     }
 
-    // Оновлюємо стиль активної кнопки
     updateActivePageStyle();
 }
 
-// Функція для оновлення стилю активної кнопки
-// Функція для оновлення стилю активної кнопки
 function updateActivePageStyle() {
     const pageButtons = document.querySelectorAll('.page_number');
 
-    // Оновлюємо стиль всіх кнопок
     pageButtons.forEach(button => {
         button.style.backgroundColor = 'rgba(0, 0, 0, 0)';
         button.style.color = 'black';
     });
 
-    // Знаходимо активну кнопку і змінюємо її стиль
     const activeButton = document.querySelector(`.page_number[data-page="${currentPage}"]`);
     if (activeButton) {
         activeButton.style.backgroundColor = 'black';
@@ -169,12 +174,8 @@ function updateActivePageStyle() {
 
 // Функція для переходу на вибрану сторінку
 function goToPage(pageNumber) {
-    currentPage = pageNumber; // Оновлюємо поточну сторінку
-
-    // Оновлюємо відображення кнопок сторінок
+    currentPage = pageNumber; 
     createPaginationButtons();
-
-    // Викликаємо функцію для запиту і відображення даних на вибраній сторінці
     fetchAndFillProductCards(currentPage);
 }
 
@@ -203,4 +204,110 @@ function changeButtonStyle(event) {
 // Додаємо обробник кліку до кожної кнопки
 pageButtons.forEach(button => {
     button.addEventListener('click', changeButtonStyle);
+});
+
+
+
+
+
+
+
+
+
+
+
+// Отримуємо посилання на елементи
+const cartToggle = document.getElementById('cartToggle');
+const closeCart = document.getElementById('closeCart');
+const basket = document.getElementById('basket');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+
+// Додаємо обробник подій для відкриття кошика
+cartToggle.addEventListener('click', () => {
+    basket.style.display = 'block'; // Показуємо кошик при кліку на корзину
+    main.style.display = 'none'; // Приховуємо <main>
+    footer.style.display = 'none'; // Приховуємо <footer>
+});
+
+// Додаємо обробник подій для закриття кошика
+closeCart.addEventListener('click', () => {
+    basket.style.display = 'none'; // Приховуємо кошик при кліку на закриття
+    main.style.display = 'block'; // Відображаємо <main>
+    footer.style.display = 'block'; // Відображаємо <footer>
+});
+
+
+const cart = {
+    items: [], // Масив продуктів у кошику
+    total: 0, // Загальна сума замовлення
+};
+
+// Функція для додавання продукту до кошика
+function addToCart(productName, productPrice) {
+    // Перевірте, чи такий продукт вже є у кошику
+    const existingProduct = cart.items.find(item => item.name === productName);
+
+    if (existingProduct) {
+        // Якщо продукт вже є у кошику, збільште його кількість
+        existingProduct.quantity += 1;
+    } else {
+        // Якщо продукта немає у кошику, додайте його
+        cart.items.push({
+            name: productName,
+            price: productPrice,
+            quantity: 1,
+        });
+    }
+
+    // Оновіть відображення кошика і обчисліть загальну суму
+    updateCartDisplay();
+    calculateTotal();
+}
+
+// Функція для оновлення відображення кошика
+function updateCartDisplay() {
+    const cartContainer = document.querySelector('.basket_content .product');
+
+    // Очистіть контейнер кошика
+    cartContainer.innerHTML = '';
+
+    // Додайте продукти з кошика до відображення
+    cart.items.forEach(item => {
+        const productElement = document.createElement('div');
+        productElement.className = 'product_information';
+        productElement.innerHTML = `
+            <p>${item.name}</p>
+            <p>${item.quantity} x ${item.price} KR.</p>
+        `;
+
+        cartContainer.appendChild(productElement);
+    });
+}
+
+// Функція для обчислення загальної суми
+function calculateTotal() {
+    cart.total = cart.items.reduce((total, item) => total + item.price * item.quantity, 0);
+
+    // Оновіть відображення загальної суми
+    const totalElement = document.querySelector('.total_sum p:last-child');
+    totalElement.textContent = `${cart.total} KR.`;
+}
+
+// Додаємо обробник подій для кнопки "ADD TO CART" для кожного продукту
+const addToCartButtons = document.querySelectorAll('.card_to_busket');
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Отримайте інформацію про продукт, на який натиснули
+        const productCard = button.closest('.product_card');
+        const productName = productCard.querySelector('.product_name p').textContent;
+
+        // Отримайте ціну продукту та перетворіть її на числовий формат
+        const productPriceText = productCard.querySelector('.condition p').textContent;
+        const productPrice = parseFloat(productPriceText);
+
+        // Додайте продукт до кошика
+        addToCart(productName, productPrice);
+    });
 });
